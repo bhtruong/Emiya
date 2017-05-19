db05e0c0cb78f248c5de8e863f9cff6b6ed0c5cf
Brian Truong <bhtruong@gmail.com>
2017-05-19 14:43:53 -0700

    Created Spring boot application and controller

---
+++ src/main/java/emiya/Application.java
@@ -0,0 +1,14 @@
+package emiya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by brian on 5/17/17.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/emiya/SetupController.java
@@ -0,0 +1,39 @@
+package emiya;
+
+import dagger.Component;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.inject.Singleton;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * Created by brian on 5/17/17.
+ */
+@RestController
+public class SetupController {
+    @Component(modules = emiya.RandomizerModule.class)
+    @Singleton
+    interface SetupDirector {
+        GameSetupDirector setupDirector();
+    }
+
+    private SetupDirector setupDirector;
+    private GameSetupDirector gameSetupDirector;
+
+    @RequestMapping(value = "/randomSetup", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Setup randomSetup(@RequestBody List<String> cardSets,
+                             @RequestParam(value="players") int players) throws SQLException
+    {
+        SetupDetails setupDetails;
+
+        setupDirector = DaggerSetupController_SetupDirector.create();
+        gameSetupDirector = setupDirector.setupDirector();
+
+        setupDetails = SetupHelper.getSetupDetails(players);
+        gameSetupDirector.setSetupDetails(setupDetails);
+
+        return gameSetupDirector.buildSetup(cardSets);
+    }
+}
